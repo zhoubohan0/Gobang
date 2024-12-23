72d2538a1662d5c605b5cb507afd4d41b7d0ee41
zhoubohan0 <3459783348@qq.com>
2024-12-23 20:33:00 +0800

    add build

---
+++ build/CMakeFiles/ai_backgammon.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zbh/Desktop/Gobang/sources/Application.cpp" "CMakeFiles/ai_backgammon.dir/sources/Application.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Application.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/ChessCore/ACEngine.cpp" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ACEngine.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ACEngine.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/ChessCore/ChessEngine.cpp" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessEngine.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessEngine.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/ChessCore/ChessManager.cpp" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessManager.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessManager.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/EventManager.cpp" "CMakeFiles/ai_backgammon.dir/sources/EventManager.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/EventManager.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/General/BenchMark.cpp" "CMakeFiles/ai_backgammon.dir/sources/General/BenchMark.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/General/BenchMark.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/General/General.cpp" "CMakeFiles/ai_backgammon.dir/sources/General/General.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/General/General.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Object/Chess.cpp" "CMakeFiles/ai_backgammon.dir/sources/Object/Chess.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Object/Chess.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Object/ChessBoard.cpp" "CMakeFiles/ai_backgammon.dir/sources/Object/ChessBoard.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Object/ChessBoard.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Object/Message.cpp" "CMakeFiles/ai_backgammon.dir/sources/Object/Message.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Object/Message.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Object/UI.cpp" "CMakeFiles/ai_backgammon.dir/sources/Object/UI.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Object/UI.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Object/highLightChess.cpp" "CMakeFiles/ai_backgammon.dir/sources/Object/highLightChess.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Object/highLightChess.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/Scene.cpp" "CMakeFiles/ai_backgammon.dir/sources/Scene.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/Scene.cpp.o.d"
+  "/home/zbh/Desktop/Gobang/sources/main.cpp" "CMakeFiles/ai_backgammon.dir/sources/main.cpp.o" "gcc" "CMakeFiles/ai_backgammon.dir/sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zbh/Desktop/Gobang/build/_deps/raylib-build/raylib/CMakeFiles/raylib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ai_backgammon.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ai_backgammon.dir/sources/Application.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Application.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ACEngine.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ACEngine.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessEngine.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessEngine.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessManager.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/ChessCore/ChessManager.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/EventManager.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/EventManager.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/General/BenchMark.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/General/BenchMark.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/General/General.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/General/General.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/Chess.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/Chess.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/ChessBoard.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/ChessBoard.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/Message.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/Message.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/UI.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/UI.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/highLightChess.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Object/highLightChess.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/Scene.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/Scene.cpp.o.d"
+  "CMakeFiles/ai_backgammon.dir/sources/main.cpp.o"
+  "CMakeFiles/ai_backgammon.dir/sources/main.cpp.o.d"
+  "ai_backgammon"
+  "ai_backgammon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ai_backgammon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
